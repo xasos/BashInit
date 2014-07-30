ef4a5901210f3986b25ee5d7f7569fd0ce486953
Niraj Pant <nirajpant7@gmail.com>
2014-07-30 00:54:17 -0500

    Actually pushes repo. API should work (found documentation online to implement)

--- make.sh
+++ make.sh
@@ -9,3 +9,7 @@ read "user?GitHub username (enter for ${gitUser}):"
 read "descript?Repo Description:"
 
 curl -u ${user:-${gitUser}} https://api.github.com/user/repos -d "{\"name\": \"${repo:-${directory}}\", \"description\": \"${descript}\", \"private\": false, \"has_issues\": true, \"has_downloads\": true, \"has_wiki\": false}"
+
+git remote set-url origin git@github.com:${user:-${gitUser}}/${repo:-${directory}}.git
+git push --set-upstream origin master
+# Done.
