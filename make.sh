90e5f41d2d3ad9497b649860ab14c0bd569e750e
Niraj Pant <nirajpant7@gmail.com>
2014-10-01 17:43:51 -0500

    Update make.sh

--- make.sh
+++ make.sh
@@ -8,8 +8,10 @@ read "repo?New git repo name (enter for ${PWD##*/}):"
 read "user?GitHub username (enter for ${gitUser}):"
 read "descript?Repo Description:"
 
+# Replace gituser with your name
+
 curl -u ${user:-${gitUser}} https://api.github.com/user/repos -d "{\"name\": \"${repo:-${directory}}\", \"description\": \"${descript}\", \"private\": false, \"has_issues\": true, \"has_downloads\": true, \"has_wiki\": false}"
 
 git remote set-url origin git@github.com:${user:-${gitUser}}/${repo:-${directory}}.git
 git push --set-upstream origin master
-# Done.
+
